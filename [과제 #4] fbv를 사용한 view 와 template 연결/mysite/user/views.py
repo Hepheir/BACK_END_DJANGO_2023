17adc11061c70c53af12cb5dd333c08b2ce6b737
Hepheir <hepheir@gmail.com>
2023-05-23 23:59:20 +0900

    feat(과제 04): 급하게 제출 ;;

--- [과제 #4] fbv를 사용한 view 와 template 연결/mysite/user/views.py
+++ [과제 #4] fbv를 사용한 view 와 template 연결/mysite/user/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
+
+def get_user(request: HttpRequest):
+    context = {
+        'posts': [
+            {
+                'content':
+            }
+        ]
+    }
+    return render(request, 'dojo/sample.html', context)
